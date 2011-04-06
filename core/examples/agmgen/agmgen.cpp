c1a20b93978b9278d1691cdbf34e6fd8f26fb162
crucis <crucis@stanford.edu>
2011-04-05 20:46:08 -0700

    agm change

--- core/examples/agmgen/agmgen.cpp
+++ core/examples/agmgen/agmgen.cpp
@@ -2,10 +2,56 @@
 //
 
 #include "stdafx.h"
+#include "../../snap/agm.h"
+
+int main(int argc, char* argv[]) {
+	Env = TEnv(argc, argv, TNotify::StdNotify);
+	Env.PrepArgs(TStr::Fmt("agmgen. build: %s, %s. Time: %s", __TIME__, __DATE__, TExeTm::GetCurTm()));
+	TExeTm ExeTm;
+	Try
+	const TStr InFNm = Env.GetIfArgPrefixStr("-d:", "DEMO", "Group affiliation data");
+	const TStr OutFPrx = Env.GetIfArgPrefixStr("-o:", "agm", "out file name prefix");
+	const int RndSeed = Env.GetIfArgPrefixFlt("-rs:",10,"Rnd Seed");
+	const double DensityCoef= Env.GetIfArgPrefixFlt("-a:",0.6,"Power-law Coeficient a of density (density ~ N^(-a)");
+	const double ScaleCoef= Env.GetIfArgPrefixFlt("-c:",1.3,"Scaling Coeficient c of density (density ~ c");
+
+	TRnd Rnd(RndSeed);
+
+	TVec<TIntV> CmtyVV;
+	if(InFNm=="DEMO") {
+		CmtyVV.Gen(2);
+		TIntV NIdV;
+		for(int i=0;i<25;i++) {
+			TIntV& CmtyV = CmtyVV[0];
+			CmtyV.Add(i+1);
+		}
+		for(int i=15;i<40;i++) {
+			TIntV& CmtyV = CmtyVV[1];
+			CmtyV.Add(i+1);
+		}
+	}
+	else {
+		TVec<TIntV> CmtyVV;
+	  TSsParser Ss(InFNm, ssfWhiteSep);
+	  while (Ss.Next()) {
+			if(Ss.GetFlds()>0) {
+				TIntV CmtyV;
+				for(int i=0;i<Ss.GetFlds();i++) {
+					if(Ss.IsInt(i)){CmtyV.Add(Ss.GetInt(i));}
+				}
+				CmtyVV.Add(CmtyV);
+			}
+	  }
+		printf("community loading completed (%d communities)\n",CmtyVV.Len());
+	}
+	PUNGraph AG = TAGM::GenAGM(CmtyVV,DensityCoef,ScaleCoef,Rnd);
+	TAGM::GVizComGraph(AG,CmtyVV,OutFPrx + ".graph.gif");
 
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-	return 0;
+	
+
+	Catch
+  printf("\nrun time: %s (%s)\n", ExeTm.GetTmStr(), TSecTm::GetCurTm().GetTmStr().CStr());
+
+  return 0;
 }
-
