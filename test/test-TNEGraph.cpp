dba10a973651a978533326e18010e38851ae84bb
Rok Sosic <rok@cs.stanford.edu>
2012-07-13 18:05:59 -0700

    added test and tutorial for TNEGraph

---
+++ test/test-TNEGraph.cpp
@@ -0,0 +1,174 @@
+#include <float.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <proc/readproc.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+#include <gtest/gtest.h>
+
+#include <snap/Snap.h>
+
+// Test the default constructor
+TEST(TNEGraph, DefaultConstructor) {
+  PNEGraph Graph;
+
+  Graph = TNEGraph::New();
+
+  EXPECT_EQ(0,Graph->GetNodes());
+  EXPECT_EQ(0,Graph->GetEdges());
+
+  EXPECT_EQ(1,Graph->IsOk());
+  EXPECT_EQ(1,Graph->Empty());
+  EXPECT_EQ(1,Graph->HasFlag(gfDirected));
+}
+
+// Test node, edge creation
+TEST(TNEGraph, ManipulateNodesEdges) {
+  int NNodes = 1000;
+  int NEdges = 100000;
+  const char *FName = "test.graph";
+
+  PNEGraph Graph;
+  PNEGraph Graph1;
+  PNEGraph Graph2;
+  int i;
+  int n;
+  int NCount;
+  int x,y;
+  int Deg, InDeg, OutDeg;
+
+  Graph = TNEGraph::New();
+  EXPECT_EQ(1,Graph->Empty());
+
+  // create the nodes
+  for (i = 0; i < NNodes; i++) {
+    Graph->AddNode(i);
+  }
+  EXPECT_EQ(0,Graph->Empty());
+  EXPECT_EQ(NNodes,Graph->GetNodes());
+
+  // create random edges
+  NCount = NEdges;
+  while (NCount > 0) {
+    x = (long) (drand48() * NNodes);
+    y = (long) (drand48() * NNodes);
+    n = Graph->AddEdge(x, y);
+    NCount--;
+  }
+
+  EXPECT_EQ(NEdges,Graph->GetEdges());
+
+  EXPECT_EQ(0,Graph->Empty());
+  EXPECT_EQ(1,Graph->IsOk());
+
+  for (i = 0; i < NNodes; i++) {
+    EXPECT_EQ(1,Graph->IsNode(i));
+  }
+
+  EXPECT_EQ(0,Graph->IsNode(NNodes));
+  EXPECT_EQ(0,Graph->IsNode(NNodes+1));
+  EXPECT_EQ(0,Graph->IsNode(2*NNodes));
+
+  // nodes iterator
+  NCount = 0;
+  for (TNEGraph::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
+    NCount++;
+  }
+  EXPECT_EQ(NNodes,NCount);
+
+  // edges per node iterator
+  NCount = 0;
+  for (TNEGraph::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
+    for (int e = 0; e < NI.GetOutDeg(); e++) {
+      NCount++;
+    }
+  }
+  EXPECT_EQ(NEdges,NCount);
+
+  // edges iterator
+  NCount = 0;
+  for (TNEGraph::TEdgeI EI = Graph->BegEI(); EI < Graph->EndEI(); EI++) {
+    NCount++;
+  }
+  EXPECT_EQ(NEdges,NCount);
+
+  // node degree
+  for (TNEGraph::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
+    Deg = NI.GetDeg();
+    InDeg = NI.GetInDeg();
+    OutDeg = NI.GetOutDeg();
+
+    EXPECT_EQ(Deg,InDeg+OutDeg);
+  }
+
+  // assignment
+  Graph1 = TNEGraph::New();
+  *Graph1 = *Graph;
+
+  EXPECT_EQ(NNodes,Graph1->GetNodes());
+  EXPECT_EQ(NEdges,Graph1->GetEdges());
+  EXPECT_EQ(0,Graph1->Empty());
+  EXPECT_EQ(1,Graph1->IsOk());
+
+  // saving and loading
+  {
+    TFOut FOut(FName);
+    Graph->Save(FOut);
+    FOut.Flush();
+  }
+
+  {
+    TFIn FIn(FName);
+    Graph2 = TNEGraph::Load(FIn);
+  }
+
+  EXPECT_EQ(NNodes,Graph2->GetNodes());
+  EXPECT_EQ(NEdges,Graph2->GetEdges());
+  EXPECT_EQ(0,Graph2->Empty());
+  EXPECT_EQ(1,Graph2->IsOk());
+
+  // remove all the nodes and edges
+  for (i = 0; i < NNodes; i++) {
+    n = Graph->GetRndNId();
+    Graph->DelNode(n);
+  }
+
+  EXPECT_EQ(0,Graph->GetNodes());
+  EXPECT_EQ(0,Graph->GetEdges());
+
+  EXPECT_EQ(1,Graph->IsOk());
+  EXPECT_EQ(1,Graph->Empty());
+
+  Graph1->Clr();
+
+  EXPECT_EQ(0,Graph1->GetNodes());
+  EXPECT_EQ(0,Graph1->GetEdges());
+
+  EXPECT_EQ(1,Graph1->IsOk());
+  EXPECT_EQ(1,Graph1->Empty());
+}
+
+// Test small graph
+TEST(TNEGraph, GetSmallGraph) {
+  PNEGraph Graph;
+
+  return;
+
+  // not implemented
+  //Graph = TNEGraph::GetSmallGraph();
+
+  EXPECT_EQ(5,Graph->GetNodes());
+  EXPECT_EQ(6,Graph->GetEdges());
+
+  EXPECT_EQ(1,Graph->IsOk());
+  EXPECT_EQ(0,Graph->Empty());
+  EXPECT_EQ(1,Graph->HasFlag(gfDirected));
+}
+
---
+++ tutorials/demo-TNEGraph.cpp
@@ -0,0 +1,138 @@
+#include <float.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <proc/readproc.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+#include <gtest/gtest.h>
+
+#include <snap/Snap.h>
+
+// Print graph statistics
+void PrintGStats(const char s[], PNEGraph Graph) {
+  printf("graph %s, nodes %d, edges %d, empty %s\n",
+      s, Graph->GetNodes(), Graph->GetEdges(),
+      Graph->Empty() ? "yes" : "no");
+}
+
+// Test the default constructor
+void DefaultConstructor() {
+  PNEGraph Graph;
+
+  Graph = TNEGraph::New();
+  PrintGStats("DefaultConstructor:Graph",Graph);
+}
+
+// Test node, edge creation
+void ManipulateNodesEdges() {
+  int NNodes = 1000;
+  int NEdges = 100000;
+  const char *FName = "test.graph";
+
+  PNEGraph Graph;
+  PNEGraph Graph1;
+  PNEGraph Graph2;
+  int i;
+  int n;
+  int NCount;
+  int ECount1;
+  int ECount2;
+  int x,y;
+  bool t;
+
+  Graph = TNEGraph::New();
+  t = Graph->Empty();
+
+  // create the nodes
+  for (i = 0; i < NNodes; i++) {
+    Graph->AddNode(i);
+  }
+  t = Graph->Empty();
+  n = Graph->GetNodes();
+
+  // create random edges
+  NCount = NEdges;
+  while (NCount > 0) {
+    x = (long) (drand48() * NNodes);
+    y = (long) (drand48() * NNodes);
+    n = Graph->AddEdge(x, y);
+    NCount--;
+  }
+  PrintGStats("ManipulateNodesEdges:Graph",Graph);
+
+  // get all the nodes
+  NCount = 0;
+  for (TNEGraph::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
+    NCount++;
+  }
+
+  // get all the edges for all the nodes
+  ECount1 = 0;
+  for (TNEGraph::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
+    for (int e = 0; e < NI.GetOutDeg(); e++) {
+      ECount1++;
+    }
+  }
+
+  // get all the edges directly
+  ECount2 = 0;
+  for (TNEGraph::TEdgeI EI = Graph->BegEI(); EI < Graph->EndEI(); EI++) {
+    ECount2++;
+  }
+  printf("graph ManipulateNodesEdges:Graph, nodes %d, edges1 %d, edges2 %d\n",
+      NCount, ECount1, ECount2);
+
+  // assignment
+  Graph1 = TNEGraph::New();
+  *Graph1 = *Graph;
+  PrintGStats("ManipulateNodesEdges:Graph1",Graph1);
+
+  // save the graph
+  {
+    TFOut FOut(FName);
+    Graph->Save(FOut);
+    FOut.Flush();
+  }
+
+  // load the graph
+  {
+    TFIn FIn(FName);
+    Graph2 = TNEGraph::Load(FIn);
+  }
+  PrintGStats("ManipulateNodesEdges:Graph2",Graph2);
+
+  // remove all the nodes and edges
+  for (i = 0; i < NNodes; i++) {
+    n = Graph->GetRndNId();
+    Graph->DelNode(n);
+  }
+
+  PrintGStats("ManipulateNodesEdges:Graph",Graph);
+
+  Graph1->Clr();
+  PrintGStats("ManipulateNodesEdges:Graph1",Graph1);
+}
+
+// Test small graph
+void GetSmallGraph() {
+  PNEGraph Graph;
+
+  return;
+
+  //Graph = TNEGraph::GetSmallGraph();
+  PrintGStats("GetSmallGraph:Graph",Graph);
+}
+
+int main(int argc, char* argv[]) {
+  DefaultConstructor();
+  ManipulateNodesEdges();
+  GetSmallGraph();
+}
+
