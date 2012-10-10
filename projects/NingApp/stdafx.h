ce4870f41c904f52b8af970214fd944010ce8a5d
Rok Sosic <rok@cs.stanford.edu>
2012-10-09 20:23:45 -0700

    cleaned up the repository - removed doc, projects, utils directories, which were moved to another repository

--- projects/NingApp/stdafx.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "targetver.h"
-
-#include <stdio.h>
-
-#include <Snap.h>
-//#include <subgraphenum.h>
-//#include <graphcounter.h>
--- utils/decision_tree/readme.txt
+++
@@ -1,1 +0,0 @@
-Simple decision tree implementation.
