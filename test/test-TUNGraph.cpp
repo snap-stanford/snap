142ba8d242874748abb1d96583ff836e80187355
Nicholas Shelly <n.shelly@acm.org>
2012-10-28 01:04:29 -0700

    Added iterations manipulate edges with random seeding.

--- test/test-TUNGraph.cpp
+++ test/test-TUNGraph.cpp
@@ -40,7 +40,7 @@ TEST(TUNGraph, ManipulateNodesEdges) {
   }
   EXPECT_EQ(0,Graph->Empty());
   EXPECT_EQ(NNodes,Graph->GetNodes());
-
+  
   // create random edges
   NCount = NEdges;
   while (NCount > 0) {
@@ -147,19 +147,13 @@ TEST(TUNGraph, ManipulateNodesEdges) {
   EXPECT_EQ(1,Graph1->Empty());
 }
 
-void print_nodes(PUNGraph Graph) {
-  printf("Nodes: ");
-  for (TUNGraph::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
-    printf("%d ", NI.GetId());
-  }
-  printf("\n");
-}
-
 // Test edge iterator, manipulate edges
 TEST(TUNGraph, ManipulateEdges) {
+  int Iterations = 100;
+  
   int NNodes;
-  int NNodesStart = 3;
-  int NNodesEnd = 50;
+  int NNodesStart = 8;
+  int NNodesEnd = 25;
   
   int NEdges;
   int NEdgesStart = 0;
@@ -172,97 +166,101 @@ TEST(TUNGraph, ManipulateEdges) {
   int x,y;
   TIntV NodeIds;
   THashSet<TIntPr> EdgeSet;
+  TInt::Rnd.PutSeed(0);
   
-  for (NEdges = NEdgesStart; NEdges <= NEdgesEnd; NEdges++) {
+  for (int i = 0; i < Iterations; i++) {
     
-    for (NNodes = NNodesStart; NNodes <= NNodesEnd; NNodes++) {
+    for (NEdges = NEdgesStart; NEdges <= NEdgesEnd; NEdges++) {
       
-      // Skip if too many edges required per NNodes (n*(n+1)/2)
-      if (NEdges > (NNodes * (NNodes+1)/2)) {
-        continue;
-      }
-      
-      Graph = TUNGraph::New();
-      EXPECT_TRUE(Graph->Empty());
-      
-      // Generate NNodes
-      NodeIds.Gen(NNodes);
-      for (int n = 0; n < NNodes; n++) {
-        NodeIds[n] = n;
-      }
-      // Add the nodes in random order
-      NodeIds.Shuffle(TInt::Rnd);
-      for (int n = 0; n < NNodes; n++) {
-        Graph->AddNode(n);
-      }
-      EXPECT_FALSE(Graph->Empty());
+      for (NNodes = NNodesStart; NNodes <= NNodesEnd; NNodes++) {
         
-      // Create random edges
-      EdgeSet.Clr();
-      NCount = NEdges;
-      while (NCount > 0) {
-        x = (long) (drand48() * NNodes);
-        y = (long) (drand48() * NNodes);
-        
-        if (!Graph->IsEdge(x,y)) {
-          Graph->AddEdge(x, y);
-          EdgeSet.AddKey(TIntPr(x, y));
-          EdgeSet.AddKey(TIntPr(y, x));
-          NCount--;
+        // Skip if too many edges required per NNodes (n*(n+1)/2)
+        if (NEdges > (NNodes * (NNodes+1)/2)) {
+          continue;
         }
-      }
-      
-      // Check edge iterator to make sure all edges are valid and no more (in hash set)
-      TIntPrV DelEdgeV;
-      for (TUNGraph::TEdgeI EI = Graph->BegEI(); EI < Graph->EndEI(); EI++) {
         
-        TIntPr Edge(EI.GetSrcNId(), EI.GetDstNId());
-        TIntPr EdgeR(EI.GetDstNId(), EI.GetSrcNId());
+        Graph = TUNGraph::New();
+        EXPECT_TRUE(Graph->Empty());
         
-        EXPECT_TRUE(EdgeSet.IsKey(Edge) || EdgeSet.IsKey(EdgeR));
-        if (EdgeSet.IsKey(Edge)) {
-            EdgeSet.DelKey(Edge);
+        // Generate NNodes
+        NodeIds.Gen(NNodes);
+        for (int n = 0; n < NNodes; n++) {
+          NodeIds[n] = n;
         }
-        if (EdgeSet.IsKey(EdgeR)) {
-          EdgeSet.DelKey(EdgeR);
+        // Add the nodes in random order
+        NodeIds.Shuffle(TInt::Rnd);
+        for (int n = 0; n < NodeIds.Len(); n++) {
+          Graph->AddNode(NodeIds[n]);
         }
-        DelEdgeV.Add(Edge);
+        EXPECT_FALSE(Graph->Empty());
         
-      }
-      EXPECT_TRUE(EdgeSet.Empty());
-      EXPECT_TRUE(DelEdgeV.Len() == NEdges);
-      
-      // Randomly delete node, check to make sure edges were deleted
-      NodeIds.Shuffle(TInt::Rnd);
-      for (int n = 0; n < NNodes; n++) {
-
-        TIntPrV DelEdgeNodeV;
-        int EdgesBeforeDel;
-        EdgesBeforeDel = Graph->GetEdges();
-        for (TUNGraph::TEdgeI EI = Graph->BegEI(); EI < Graph->EndEI(); EI++) {
-          if (EI.GetSrcNId() == n || EI.GetDstNId() == n) {
-            DelEdgeNodeV.Add(TIntPr(EI.GetSrcNId(), EI.GetDstNId()));
+        // Create random edges
+        EdgeSet.Clr();
+        NCount = NEdges;
+        while (NCount > 0) {
+          x = (long) (drand48() * NNodes);
+          y = (long) (drand48() * NNodes);
+          
+          if (!Graph->IsEdge(x,y)) {
+            Graph->AddEdge(x, y);
+            EdgeSet.AddKey(TIntPr(x, y));
+            EdgeSet.AddKey(TIntPr(y, x));
+            NCount--;
           }
         }
-        Graph->DelNode(n);
-        EXPECT_TRUE(EdgesBeforeDel == DelEdgeNodeV.Len() + Graph->GetEdges());
-        EXPECT_FALSE(Graph->IsNode(n));
-        EXPECT_TRUE(Graph->IsOk());
         
-        // Make sure all the edges to deleted node are gone
-        for (int e = 0; e < DelEdgeNodeV.Len(); e++) {
-          EXPECT_FALSE(Graph->IsEdge(DelEdgeNodeV[e].Val1, DelEdgeNodeV[e].Val2));
-        }
-        
-        // Make sure no edge on graph is connected to deleted node
+        // Check edge iterator to make sure all edges are valid and no more (in hash set)
+        TIntPrV DelEdgeV;
         for (TUNGraph::TEdgeI EI = Graph->BegEI(); EI < Graph->EndEI(); EI++) {
-          EXPECT_FALSE(EI.GetSrcNId() == n || EI.GetDstNId() == n);
+          
+          TIntPr Edge(EI.GetSrcNId(), EI.GetDstNId());
+          TIntPr EdgeR(EI.GetDstNId(), EI.GetSrcNId());
+          
+          EXPECT_TRUE(EdgeSet.IsKey(Edge) || EdgeSet.IsKey(EdgeR));
+          if (EdgeSet.IsKey(Edge)) {
+              EdgeSet.DelKey(Edge);
+          }
+          if (EdgeSet.IsKey(EdgeR)) {
+            EdgeSet.DelKey(EdgeR);
+          }
+          DelEdgeV.Add(Edge);
+          
         }
-      }
+        EXPECT_TRUE(EdgeSet.Empty());
+        EXPECT_TRUE(DelEdgeV.Len() == NEdges);
+        
+        // Randomly delete node, check to make sure edges were deleted
+        NodeIds.Shuffle(TInt::Rnd);
+        for (int n = 0; n < NNodes; n++) {
 
-      EXPECT_TRUE(0 == Graph->GetEdges());
-      Graph->Clr();
-      EXPECT_TRUE(Graph->Empty());
+          TIntPrV DelEdgeNodeV;
+          int EdgesBeforeDel;
+          EdgesBeforeDel = Graph->GetEdges();
+          for (TUNGraph::TEdgeI EI = Graph->BegEI(); EI < Graph->EndEI(); EI++) {
+            if (EI.GetSrcNId() == n || EI.GetDstNId() == n) {
+              DelEdgeNodeV.Add(TIntPr(EI.GetSrcNId(), EI.GetDstNId()));
+            }
+          }
+          Graph->DelNode(n);
+          EXPECT_TRUE(EdgesBeforeDel == DelEdgeNodeV.Len() + Graph->GetEdges());
+          EXPECT_FALSE(Graph->IsNode(n));
+          EXPECT_TRUE(Graph->IsOk());
+          
+          // Make sure all the edges to deleted node are gone
+          for (int e = 0; e < DelEdgeNodeV.Len(); e++) {
+            EXPECT_FALSE(Graph->IsEdge(DelEdgeNodeV[e].Val1, DelEdgeNodeV[e].Val2));
+          }
+          
+          // Make sure no edge on graph is connected to deleted node
+          for (TUNGraph::TEdgeI EI = Graph->BegEI(); EI < Graph->EndEI(); EI++) {
+            EXPECT_FALSE(EI.GetSrcNId() == n || EI.GetDstNId() == n);
+          }
+        }
+
+        EXPECT_TRUE(0 == Graph->GetEdges());
+        Graph->Clr();
+        EXPECT_TRUE(Graph->Empty());
+      }
     }
   }
 
