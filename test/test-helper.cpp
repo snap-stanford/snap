e9411dbdbc21b34aeb1f6e5fefdcf319da9ebe4a
Nicholas Shelly <n.shelly@acm.org>
2013-01-06 00:28:46 -0700

    Added baseline files for graph visualization.

---
+++ test/test-helper.cpp
@@ -0,0 +1,9 @@
+//
+//  test-helper.cpp
+//  snap-test
+//
+//  Created by Nicholas Shelly on 12/30/12.
+//  Copyright (c) 2012 infolab. All rights reserved.
+//
+
+#include "test-helper.h"
---
+++ test/test-helper.h
@@ -0,0 +1,14 @@
+//
+//  test-helper.h
+//  snap-test
+//
+//  Created by Nicholas Shelly on 12/30/12.
+//  Copyright (c) 2012 infolab. All rights reserved.
+//
+
+#ifndef __snap_test__test_helper__
+#define __snap_test__test_helper__
+
+#include <iostream>
+
+#endif /* defined(__snap_test__test_helper__) */
