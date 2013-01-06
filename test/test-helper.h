305b0da1ae74ac62fd667e7d13d24b8ad55f95c9
Nicholas Shelly <n.shelly@acm.org>
2013-01-06 00:29:41 -0700

    Created helper files for file IO tests.

--- test/test-helper.h
+++ test/test-helper.h
@@ -2,13 +2,20 @@
 //  test-helper.h
 //  snap-test
 //
+//  Utility functions for tests.
 //  Created by Nicholas Shelly on 12/30/12.
-//  Copyright (c) 2012 infolab. All rights reserved.
 //
 
 #ifndef __snap_test__test_helper__
 #define __snap_test__test_helper__
 
-#include <iostream>
+#include "Snap.h"
+
+//#include <stdio.h>
+#include <string>
+
+bool fileExists(const std::string& filename);
+bool compareFiles(const std::string& f1, const std::string& f2);
+void printv(TIntV IntV);
 
 #endif /* defined(__snap_test__test_helper__) */
