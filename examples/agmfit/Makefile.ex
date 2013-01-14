66aa76dc932e75643fa7717acfb90162d98e95b1
jayang <augustxmas@gmail.com>
2013-01-13 20:17:25 -0800

    add agmfit example

---
+++ examples/agmfit/Makefile.ex
@@ -0,0 +1,8 @@
+#
+#	configuration variables for the example
+
+## Main application file
+MAIN = agmfitmain
+DEPH = $(EXSNAPADV)/agm.h $(EXSNAPADV)/agmfit.h
+DEPCPP = $(EXSNAPADV)/agm.cpp $(EXSNAPADV)/agmfit.cpp
+
