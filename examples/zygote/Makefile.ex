423d5cbc8c68a26390a03f7fff72b3b57066262f
Rok Sosic <rok@cs.stanford.edu>
2013-04-30 19:01:51 -0700

    added zydemo example that demonstrates the use of libzygote

---
+++ examples/zygote/Makefile.ex
@@ -0,0 +1,47 @@
+#
+#	configuration variables for the example
+
+## Main application file
+MAIN = zydemo
+DEPH = 
+DEPCPP = 
+
+SNAPDIR = $(EXSNAP)
+GLIBDIR = $(EXGLIB)
+
+ZYDIR = ../../..
+LIBZYGOTE = $(ZYDIR)/libzygote
+CXXFLAGS += -I$(LIBZYGOTE)
+LDFLAGS += -L$(LIBZYGOTE)
+
+LIBS += -lzygote
+
+UNAME := $(shell uname)
+
+ifeq ($(UNAME), Linux)
+	# Linux flags
+	CXXLIBFLAGS = $(CXXFLAGS) -fPIC -D__STDC_LIMIT_MACROS
+	LDLIBFLAGS = $(LDFLAGS) -shared
+else ifeq ($(UNAME), Darwin)
+	# OS X flags
+	LDLIBFLAGS = $(LDFLAGS) -dynamiclib
+else ifeq ($(shell uname -o), Cygwin)
+	# Cygwin flags
+	CXXLIBFLAGS += $(CXXFLAGS) -shared -D__STDC_LIMIT_MACROS
+endif
+
+all: $(MAIN).so
+
+%.so: %.o $(DEPH) $(DEPCPP) Snap.o
+	$(CXX) $(CXXFLAGS) $(CXXLIBFLAGS) $(LDLIBFLAGS) -o $@ $^ $(LIBS)
+
+%.o: %.cpp
+	$(CXX) -c -o $@ $^ $(CXXLIBFLAGS) -I$(EXSNAP) -I$(EXSNAPADV) -I$(EXGLIB) -I$(EXSNAPEXP)
+
+Snap.o: 
+	$(CXX) $(CXXLIBFLAGS) -c $(SNAPDIR)/Snap.cpp -I$(SNAPDIR) -I$(GLIBDIR)
+
+clean: clean-so
+
+clean-so:
+	rm -f $(MAIN).so
