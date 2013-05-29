4aec9bc2284a9513c3886d92bede6ce3af26f86f
Jason Jong <jjong@stanford.edu>
2013-05-29 16:41:10 -0700

    Remove /benchmarking.  Moved to /ringo

--- benchmarking/other_platforms/SQL/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sizes=( 10 30 100 300 1000 3000 10000 )
-for i in "${sizes[@]}"
-do
-  echo "Building graphs with input size $i (thousands of rows)"
-  sed "s/SIZEPARAM/$i/" query_test.sql | mysql -u mraison -p mraison
-done
-
