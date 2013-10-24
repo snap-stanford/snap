845adb4693b0772ae5d0ceaedc754b270daa8ed2
Rok Sosic <rok@cs.stanford.edu>
2013-10-24 10:58:56 -0700

    fixed a problem in TNEANet::DelEdge()

--- snap-core/network.cpp
+++ snap-core/network.cpp
@@ -448,9 +448,10 @@ void TNEANet::DelEdge(const int& EId) {
 
 // delete all edges between the two nodes
 void TNEANet::DelEdge(const int& SrcNId, const int& DstNId, const bool& IsDir) {
-  int EId = IsEdge(SrcNId, DstNId, EId, IsDir);
-  IAssert(EId); // there is at least one edge
-  while (IsEdge(SrcNId, DstNId, EId, IsDir)) {
+  int EId = 0;
+  bool Edge = IsEdge(SrcNId, DstNId, EId, IsDir);
+  IAssert(Edge); // there is at least one edge
+  while (Edge) {
     GetNode(SrcNId).OutEIdV.DelIfIn(EId);
     GetNode(DstNId).InEIdV.DelIfIn(EId);
   }
