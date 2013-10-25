9ad785cbd122e7a7ccda10f41c62c9e57b4c5568
yonathan perez <yperez@stanford.edu>
2013-10-25 02:51:31 -0700

    changed calls to LoadSS in test to comply with recent prototye changes

--- ringo/SetOperationsTest.cpp
+++ ringo/SetOperationsTest.cpp
@@ -14,8 +14,8 @@ int main(){
   RelevantCols.Add(2);
   RelevantCols.Add(3);
 
-  PTable P = TTable::LoadSS("Animals", AnimalS, "tests/animals.txt", Context, RelevantCols);
-  PTable Q = TTable::LoadSS("MoreAnimals", AnimalS, "tests/more_animals.txt", Context, RelevantCols);
+  PTable P = TTable::LoadSS("Animals", AnimalS, "tests/animals.txt", Context, RelevantCols, '\t', true);
+  PTable Q = TTable::LoadSS("MoreAnimals", AnimalS, "tests/more_animals.txt", Context, RelevantCols, '\t', true);
 
   PTable R = P->Union(*Q, "union");
   PTable S = P->Intersection(*Q, "intersection");
