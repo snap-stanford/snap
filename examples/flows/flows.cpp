095fc3d6169c7f675171510af92f373b6bd1ac86
David Zeng <dzeng0@madmax6.stanford.edu>
2014-03-13 10:32:06 -0700

    small changes to examples file

--- examples/flows/flows.cpp
+++ examples/flows/flows.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include <omp.h>
 
 int BuildCapacityNetwork(const TStr& InFNm, PNEANet &Net, const int& SrcColId = 0, const int& DstColId = 1, const int& CapColId = 2) {
   TSsParser Ss(InFNm, ssfWhiteSep, true, true, true);
@@ -9,8 +10,8 @@ int BuildCapacityNetwork(const TStr& InFNm, PNEANet &Net, const int& SrcColId = 
   int MaxCap = 0;
   while (Ss.Next()) {
     if (! Ss.GetInt(SrcColId, SrcNId) || ! Ss.GetInt(DstColId, DstNId)) { continue; }
-    Ss.GetInt(CapColId, CapVal);
-    //CapVal = Random.GetUniDevInt(0, 10000);
+    //Ss.GetInt(CapColId, CapVal);
+    CapVal = Random.GetUniDevInt(1, 10000);
     MaxCap = max(CapVal, MaxCap);
     if (! Net->IsNode(SrcNId)) {
       Net->AddNode(SrcNId);
@@ -28,7 +29,7 @@ int BuildCapacityNetwork(const TStr& InFNm, PNEANet &Net, const int& SrcColId = 
 double getcputime() {
   struct rusage rusage;
   double result;
-  getrusage(RUSAGE_THREAD, &rusage);
+  getrusage(RUSAGE_SELF, &rusage);
   result =
     ((double) (rusage.ru_utime.tv_usec + rusage.ru_stime.tv_usec) / 1000000) +
     ((double) (rusage.ru_utime.tv_sec + rusage.ru_stime.tv_sec));
@@ -42,52 +43,58 @@ int main(int argc, char* argv[]) {
   double NetEKTimeSum = 0;
   int NumWins = 0;
   Try
-  const TStr InFNm = Env.GetIfArgPrefixStr("-i:", "small.txt", "Input directed graph");
+  const TStr InFNm = Env.GetIfArgPrefixStr("-i:", "small_bin", "Input file");
+  //const TStr OutFNm = Env.GetIfArgPrefixStr("-o:", "small_bin", "Output file");
   const int Iters = Env.GetIfArgPrefixInt("-n:", 10, "Number of runs per thread");
   const int Threads = Env.GetIfArgPrefixInt("-t:", 4, "Number of threads");
   printf("Integer Flow Test\n");
   printf("Filename: %s\n", InFNm.CStr());
   printf("Building Network...\n");
-  PNEANet Net;
-  int MaxEdgeCap = BuildCapacityNetwork(InFNm, Net);
-  printf("PNEANet Nodes: %d, Edges: %d, Max Edge Capacity: %d\n", Net->GetNodes(), Net->GetEdges(), MaxEdgeCap);
+  TFIn InFile(InFNm);
+  PNEANet Net = TNEANet::Load(InFile);
+  //int MaxEdgeCap = BuildCapacityNetwork(InFNm, Net);
+  //TFOut OutFile(OutFNm);
+  //Net->Save(OutFile);
+  printf("PNEANet Nodes: %d, Edges: %d\n\n", Net->GetNodes(), Net->GetEdges());
   #pragma omp parallel for reduction(+:NetEKTimeSum,NetPRTimeSum,NumWins) schedule(static, 1)
   for (int t = 0; t < Threads; t++) {
-    TRnd Random(t+1);
+    TRnd Random(t);
     for (int i = 0; i < Iters; i++) {
       int SrcNId = Net->GetRndNId(Random);
       int SnkNId = Net->GetRndNId(Random);
 
-      double PRBeginTime = getcputime();
-      int NetMaxFlowPR = TSnap::GetMaxFlowIntPR(Net, SrcNId, SnkNId);
-      double PREndTime = getcputime();
-      double NetPRFlowRunTime = PREndTime - PRBeginTime;
+      //double PRBeginTime = getcputime();
+      //int NetMaxFlowPR = TSnap::GetMaxFlowIntPR(Net, SrcNId, SnkNId);
+      //double PREndTime = getcputime();
+      //double NetPRFlowRunTime = PREndTime - PRBeginTime;
 
       double EKBeginTime = getcputime();
       int NetMaxFlowEK = TSnap::GetMaxFlowIntEK(Net, SrcNId, SnkNId);
       double EKEndTime = getcputime();
       double NetEKFlowRunTime = EKEndTime - EKBeginTime;
+      
+      //IAssert(NetMaxFlowPR == NetMaxFlowEK);
 
-      if (NetPRFlowRunTime < NetEKFlowRunTime) { NumWins++; }
+      //if (NetPRFlowRunTime < NetEKFlowRunTime) { NumWins++; }
 
-      NetPRTimeSum += NetPRFlowRunTime;
+      //NetPRTimeSum += NetPRFlowRunTime;
       NetEKTimeSum += NetEKFlowRunTime;
-
+      
       #pragma omp critical
       {
+        printf("Thread: %d\n", omp_get_thread_num());
         printf("Source: %d, Sink %d\n", SrcNId, SnkNId);
-        printf("Max Flow PR: %d\n", NetMaxFlowPR);
-        printf("run time: %f\n", NetPRFlowRunTime);
-        printf("Max Flow EK: %d\n", NetMaxFlowEK);
-        printf("run time: %f\n", NetEKFlowRunTime);
+        printf("Max Flow: %d\n", NetMaxFlowEK);
+        //printf("PR CPU Time: %f\n", NetPRFlowRunTime);
+        printf("EK CPU Time: %f\n", NetEKFlowRunTime);
         printf("\n");
       }
     }
   }
   int TotalRuns = Iters*Threads;
-  printf ("\nAvg PR PNEANet Time: %f\n", NetPRTimeSum/TotalRuns);
+  //printf ("Avg PR PNEANet Time: %f\n", NetPRTimeSum/TotalRuns);
   printf ("Avg EK PNEANet Time: %f\n", NetEKTimeSum/TotalRuns);
-  printf ("%d out of %d PR was faster\n", NumWins, TotalRuns);
+  //printf ("%d out of %d PR was faster\n", NumWins, TotalRuns);
   Catch
   return 0;
 }
