8005592eed40f77ad5cf829b03dda2e7897fdee8
guotai <taig@stanford.edu>
2014-03-19 14:31:26 -0700

    change example

--- examples/rolx/prototype.cpp
+++ examples/rolx/prototype.cpp
@@ -3,36 +3,37 @@
 #include "rolx.h"
 
 int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    printf("Usage: ./prototype <dataset file>\n");
+  if (argc < 4) {
+    printf("Usage: ./prototype <dataset file> <min roles> <max roles>\n");
     exit(EXIT_SUCCESS);
   }
-
-  PUNGraph Graph = TSnap::LoadEdgeList<PUNGraph>(
-      argv[1], 0, 1);
-  printf("finish loading file\n");
-
+  int min_roles = atoi(argv[2]);
+  int max_roles = atoi(argv[3]);
+  if (min_roles > max_roles || min_roles < 2) {
+    printf("min roles and max roles should be integer and\n");
+	printf("2 <= min roles <= max roles\n");
+	exit(EXIT_SUCCESS);
+  }
+  printf("loading file...\n");
+  PUNGraph Graph = TSnap::LoadEdgeList<PUNGraph>(argv[1], 0, 1);
+  printf("extracting features...\n");
   TIntFtrH Features = ExtractFeatures(Graph);
-  printf("finish feature extraction\n");
-  printf("--features--\n");
-  //PrintFeatures(Features);
   TIntIntH NodeIdMtxIdH = CreateNodeIdMtxIdxHash(Features);
-  printf("--finish create (node ID -> Mtx ID) hash--\n");
-  TFltVV V = ConvertFeatureToMatrix(Features, NodeIdMtxIdH); 
-  printf("--finish convert feature to matrix--\n");
-  //printf("--feature matrix--\n");
-  //FPrintMatrix(V, "v.txt");
-
+  TFltVV V = ConvertFeatureToMatrix(Features, NodeIdMtxIdH);
+  printf("saving features...\n");
+  FPrintMatrix(V, "v.txt");
+  printf("feature matrix is saved in v.txt\n");
   TFlt MnError = TFlt::Mx;
   TFltVV FinalG, FinalF;
   int NumRoles = -1;
-  for (int r = 3; r < 4; ++r) {
+  for (int r = min_roles; r <= max_roles; ++r) {
     TFltVV G, F;
+	printf("factorizing for %d roles...\n", r);
     time_t t = time(NULL);
     CalcNonNegativeFactorization(V, r, G, F);
-    printf("Factorization use %f seconds\n", difftime(time(NULL), t));
-    printf("finish factorization for r=%d\n", r);
-    //PrintMatrix(G);
+    printf("Factorization uses %f seconds\n", difftime(time(NULL), t));
+    //FPrintMatrix(G, "g.txt");
+	//FPrintMatrix(F, "f.txt");
     TFlt Error = CalcDescriptionLength(V, G, F);
     if (Error < MnError) {
       MnError = Error;
@@ -41,15 +42,11 @@ int main(int argc, char* argv[]) {
       NumRoles = r;
     }
   }
-  //printf("--FinalG--\n");
-  //PrintMatrix(FinalG);
-  //printf("--FinalF--\n");
-  //PrintMatrix(FinalF);
+  //FPrintMatrix(FinalG, "final_g.txt");
+  //FPrintMatrix(FinalF, "final_f.txt");
   printf("using %d roles, min error: %f\n", NumRoles, MnError());
-
   TIntIntH Roles = FindRoles(FinalG, NodeIdMtxIdH);
   FPrintRoles(Roles, "roles.txt");
   //PlotRoles(Graph, Roles);
-
   return 0;
 }
