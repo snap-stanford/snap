e8ccdf38ef9be9eddbefb75bda0bf4b8b77dd5ac
Cobra Kao <ru6xul6@gmail.com>
2014-03-20 14:52:51 -0700

    Rename main function to testrolx.cpp

--- examples/rolx/Makefile.ex
+++ examples/rolx/Makefile.ex
@@ -2,7 +2,7 @@
 #	configuration variables for the example
 
 ## Main application file
-MAIN = prototype
+MAIN = testrolx
 DEPH = $(EXSNAPADV)/rolx.h
 DEPCPP = $(EXSNAPADV)/rolx.cpp
 CXXFLAGS = -fopenmp
