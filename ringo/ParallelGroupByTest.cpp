9bbd132a113be34540599d7dd562f94cb826330f
yonathan perez <yperez@stanford.edu>
2014-11-14 03:47:55 -0800

    indentation fix

--- ringo/ParallelGroupByTest.cpp
+++ ringo/ParallelGroupByTest.cpp
@@ -4,17 +4,17 @@ A simple test for parallel GroupBy
 */
 
 int main(int argc, char** argv){
-	TStr infile = "/lfs/madmax2/0/yonathan/soc-LiveJournal1-noheader.txt";
-	Schema S;
-	S.Add(TPair<TStr,TAttrType>("src", atInt));
-	S.Add(TPair<TStr,TAttrType>("dst", atInt));
-	TTableContext Context;
-	TTable::SetMP(true);
+  TStr infile = "/lfs/madmax2/0/yonathan/soc-LiveJournal1-noheader.txt";
+  Schema S;
+  S.Add(TPair<TStr,TAttrType>("src", atInt));
+  S.Add(TPair<TStr,TAttrType>("dst", atInt));
+  TTableContext Context;
+  TTable::SetMP(true);
 	
-	PTable Edges = TTable::LoadSS(S, infile, Context);
+  PTable Edges = TTable::LoadSS(S, infile, Context);
 	
-	THashMP<TInt, TIntV> Grouping(5000000);
-	Edges->GroupByIntColMP("src", Grouping, true);
-	return 0;
+  THashMP<TInt, TIntV> Grouping(5000000);
+  Edges->GroupByIntColMP("src", Grouping, true);
+  return 0;
 }
 	
