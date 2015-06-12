db14eff1070f90883dc967499dd7240dbb2ca77e
dsardina <dsardina@dmi.unict.it>
2015-06-12 15:49:59 +0200

    Added folder with the project

---
+++ contrib/universityofcatania_verona-risnap/RI-DSToSnap/rids.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <cstdlib>
+#include "Snap.h"
+#include "rids.h"
---
+++ contrib/universityofcatania_verona-risnap/RiToSnap/rinetmatch.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <stdio.h>
+#include <stdlib.h>
+#include "Snap.h"
+#include "rinetmatch.h"
+
+
+
+
