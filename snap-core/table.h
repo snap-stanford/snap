4a055f31688546d3b24dfc5f79be06d3e78a9130
Rok Sosic <rok@cs.stanford.edu>
2015-11-17 19:23:44 -0800

    - included tmetric.h in table.h

--- snap-core/table.h
+++ snap-core/table.h
@@ -1,10 +1,33 @@
 #ifndef TABLE_H
 #define TABLE_H
-#include "tmetric.h"
-//#include "snap.h"
+
 #ifdef USE_OPENMP
 #define CHUNKS_PER_THREAD 10
 #endif
+
+//#//////////////////////////////////////////////
+/// Metric class: base class for distance metrics
+class TMetric {
+protected:
+  TStr MetricName; ///< Name of the metric defined by this class
+public:
+  TMetric(TStr Name) : MetricName(Name) {}
+  /// Get the name of this metric
+  TStr GetName();
+  /// Virtual base function for defining metric on floats
+  virtual TFlt NumDist(TFlt,TFlt) { return -1; }
+  /// Virtual base function for defining metric on strings
+  virtual TFlt StrDist(TStr,TStr) { return -1; }
+};
+
+//#//////////////////////////////////////////////
+/// Euclidean metric class: compute distance between two floats
+class TEuclideanMetric: public TMetric {
+public:
+  TEuclideanMetric(TStr Name) : TMetric(Name) {}
+  /// Calculate the euclidean distance of two floats
+  TFlt NumDist(TFlt x1,TFlt x2) { return fabs(x1-x2); }
+};
 
 /// Distance metrics for similarity joins
 // Haversine distance is used to calculate distance between two points on a sphere based on latitude and longitude
