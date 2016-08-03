cd02f98b8a57cef20caccb50181c42eee6f5ca71
Vid Kocijan <koci@ilws16.stanford.edu>
2016-08-03 12:03:30 -0700

    Multithreading for optimization added.

--- snap-exp/node2vec-dev/word2vec.cpp
+++ snap-exp/node2vec-dev/word2vec.cpp
@@ -74,74 +74,120 @@ void InitPosEmb(TIntV& Vocab, int& Dimensions, TRnd& Rnd, TFltVV& SynPos) {
     }
   }
 }
+//for passing data to each thread
+struct TThreadDat {
+  TIntVV& WalksVV;
+  int& Dimensions;
+  int& WinSize;
+  int& Iter;
+  TRnd& Rnd;
+  TIntV& KTable;
+  TFltV& UTable;
+  int& WordCntAll;
+  TFltV& ExpTable;
+  double& Alpha;
+  double& StartAlpha;
+  int& MAX_EXP;
+  int& EXP_TABLE_PRECISION;
+  int& TABLE_SIZE;
+  int FirstWalk;
+  int LastWalk;
+  TFltVV& SynNeg;
+  TFltVV& SynPos;
+  TThreadDat (TIntVV& WalksVV, int& Dimensions, int& WinSize, int& Iter, TRnd& Rnd,
+   TIntV& KTable, TFltV& UTable, int& WordCntAll, TFltV& ExpTable, double& Alpha,
+   double& StartAlpha, int& MAX_EXP, int& EXP_TABLE_PRECISION, int& TABLE_SIZE,
+   int FirstWalk, int LastWalk, TFltVV& SynNeg, TFltVV& SynPos) : 
+    WalksVV(WalksVV), Dimensions(Dimensions), WinSize(WinSize), Iter(Iter), Rnd(Rnd),
+    KTable(KTable), UTable(UTable), WordCntAll(WordCntAll), ExpTable(ExpTable), 
+    Alpha(Alpha), StartAlpha(StartAlpha), MAX_EXP(MAX_EXP),
+    EXP_TABLE_PRECISION(EXP_TABLE_PRECISION), TABLE_SIZE(TABLE_SIZE),
+    FirstWalk(FirstWalk), LastWalk(LastWalk), SynNeg(SynNeg), SynPos(SynPos) {}
+};
 
-void TrainModel(TIntV& WalkV, TIntV& Vocab, int& Dimensions, int& WinSize,
- int& Iter, TRnd& Rnd, TIntV& KTable, TFltV& UTable, int& WordCntAll,
- TFltV& ExpTable, double& Alpha, double& StartAlpha, int AllWords,
- TFltVV& SynNeg, TFltVV& SynPos) {
+void* TrainModel(void* ThrDat) {
+  TThreadDat* Dat = (TThreadDat*) ThrDat;
+  TIntVV& WalksVV = Dat->WalksVV;
+  TRnd& Rnd = Dat->Rnd;
+  TFltV& ExpTable = Dat->ExpTable;
+  int& Dimensions = Dat->Dimensions;
+  int& WinSize = Dat->WinSize;
+  int& WordCntAll = Dat->WordCntAll;
+  double& Alpha = Dat->Alpha;
+  double& StartAlpha = Dat->StartAlpha;
+  int& MAX_EXP = Dat->MAX_EXP, EXP_TABLE_PRECISION = Dat->EXP_TABLE_PRECISION;
+  int& TABLE_SIZE = Dat->TABLE_SIZE;
+  TFltVV& SynNeg = Dat->SynNeg;
+  TFltVV& SynPos = Dat->SynPos;
+  int& FirstWalk = Dat->FirstWalk;
+  int& LastWalk = Dat->LastWalk;
   TFltV Neu1V(Dimensions);
   TFltV Neu1eV(Dimensions);
-  int MAX_EXP = 6, EXP_TABLE_PRECISION = 1000;//There are 2 copies of this
-  int TABLE_SIZE = MAX_EXP * EXP_TABLE_PRECISION * 2;
-  int LocalIter = Iter, WordI = 0, NegSamN = 5;//15;
-  while(1) {
-    if ( WordCntAll%10000 == 0 ) {
-      printf("%cAlpha: %lf  Progress: %.2lf%% ",13,Alpha,(double)WordCntAll*100/(double)(Iter*AllWords));fflush(stdout);
-      Alpha = StartAlpha * (1 - WordCntAll / static_cast<double>(Iter * AllWords + 1));
-      if ( Alpha < StartAlpha * 0.0001 ) { Alpha = StartAlpha * 0.0001; }
-    }
-    int Word = WalkV[WordI];
-    for (int i = 0; i < Dimensions; i++) {
-      Neu1V[i] = 0;
-      Neu1eV[i] = 0;
-    }
-    int Offset = Rnd.GetUniDevInt() % WinSize;
-    for (int a = Offset; a < WinSize * 2 + 1 - Offset; a++) {
-      if (a == WinSize) { continue; }
-      int CurrWordI = WordI - WinSize + a;
-      if (CurrWordI < 0){ continue; }
-      if (CurrWordI >= WalkV.Len()){ continue; }
-      int CurrWord = WalkV[CurrWordI];
-      for (int i = 0; i < Dimensions; i++) { Neu1eV[i] = 0; }
-      //negative sampling
-      for (int j = 0; j < NegSamN+1; j++) {
-        int Target, Label;
-        if (j == 0) {
-          Target = Word;
-          Label = 1;
-        } else {
-          Target = RndUnigramInt(KTable, UTable, Rnd);
-          if (Target == Word) { continue; }
-          Label = 0;
-        }
-        double Product = 0;
-        for (int i = 0; i < Dimensions; i++) {
-          Product += SynPos(CurrWord,i) * SynNeg(Target,i);
-        }
-        double Grad;//Gradient multiplied by learning rate
-        if (Product > MAX_EXP) { Grad = (Label - 1) * Alpha; }
-        else if (Product < -MAX_EXP) { Grad = Label * Alpha; }
-        else { 
-          double Exp = ExpTable[static_cast<int>(Product*EXP_TABLE_PRECISION)+TABLE_SIZE/2];
-          Grad = (Label - 1 + 1 / (1 + Exp)) * Alpha;
-        }
-        for (int i = 0; i < Dimensions; i++) { 
-          Neu1eV[i] += Grad * SynNeg(Target,i);
-          SynNeg(Target,i) += Grad * SynPos(CurrWord,i);
-        }
+  int AllWords = WalksVV.GetXDim()*WalksVV.GetYDim();
+  for (int CurrWalk = FirstWalk; CurrWalk <= LastWalk; CurrWalk++) {
+    int LocalIter = Dat->Iter, WordI = 0, NegSamN = 5;//15;
+    TIntV WalkV(WalksVV.GetYDim());
+    for (int j = 0; j < WalksVV.GetYDim(); j++) { WalkV[j] = WalksVV(CurrWalk,j); }
+    while(1) {
+      if ( WordCntAll%10000 == 0 ) {
+        printf("%cAlpha: %lf  Progress: %.2lf%% ",13,Alpha,(double)WordCntAll*100/(double)(Dat->Iter*AllWords));fflush(stdout);
+        Alpha = StartAlpha * (1 - WordCntAll / static_cast<double>(Dat->Iter * AllWords + 1));
+        if ( Alpha < StartAlpha * 0.0001 ) { Alpha = StartAlpha * 0.0001; }
       }
+      int Word = WalkV[WordI];
       for (int i = 0; i < Dimensions; i++) {
-        SynPos(CurrWord,i) += Neu1eV[i];
+        Neu1V[i] = 0;
+        Neu1eV[i] = 0;
       }
-    }
-    WordCntAll++;
-    WordI++;
-    if (WordI >= WalkV.Len()) {
-      LocalIter--;
-      WordI = 0;
-      if(LocalIter == 0) { break; }
+      int Offset = Rnd.GetUniDevInt() % WinSize;
+      for (int a = Offset; a < WinSize * 2 + 1 - Offset; a++) {
+        if (a == WinSize) { continue; }
+        int CurrWordI = WordI - WinSize + a;
+        if (CurrWordI < 0){ continue; }
+        if (CurrWordI >= WalkV.Len()){ continue; }
+        int CurrWord = WalkV[CurrWordI];
+        for (int i = 0; i < Dimensions; i++) { Neu1eV[i] = 0; }
+        //negative sampling
+        for (int j = 0; j < NegSamN+1; j++) {
+          int Target, Label;
+          if (j == 0) {
+            Target = Word;
+            Label = 1;
+          } else {
+            Target = RndUnigramInt(Dat->KTable, Dat->UTable, Rnd);
+            if (Target == Word) { continue; }
+            Label = 0;
+          }
+          double Product = 0;
+          for (int i = 0; i < Dimensions; i++) {
+            Product += SynPos(CurrWord,i) * SynNeg(Target,i);
+          }
+          double Grad;//Gradient multiplied by learning rate
+          if (Product > MAX_EXP) { Grad = (Label - 1) * Alpha; }
+          else if (Product < -MAX_EXP) { Grad = Label * Alpha; }
+          else { 
+            double Exp = ExpTable[static_cast<int>(Product*EXP_TABLE_PRECISION)+TABLE_SIZE/2];
+            Grad = (Label - 1 + 1 / (1 + Exp)) * Alpha;
+          }
+          for (int i = 0; i < Dimensions; i++) { 
+            Neu1eV[i] += Grad * SynNeg(Target,i);
+            SynNeg(Target,i) += Grad * SynPos(CurrWord,i);
+          }
+        }
+        for (int i = 0; i < Dimensions; i++) {
+          SynPos(CurrWord,i) += Neu1eV[i];
+        }
+      }
+      WordCntAll++;
+      WordI++;
+      if (WordI >= WalkV.Len()) {
+        LocalIter--;
+        WordI = 0;
+        if(LocalIter == 0) { break; }
+      }
     }
   }
+  pthread_exit(NULL);
 }
 
 
@@ -172,7 +218,7 @@ void LearnEmbeddings(TIntVV& WalksVV, int& Dimensions, int& WinSize,
   InitPosEmb(Vocab, Dimensions, Rnd, SynPos);
   InitNegEmb(Vocab, Dimensions, SynNeg);
   InitUnigramTable(Vocab, KTable, UTable);
-  int MAX_EXP = 6, EXP_TABLE_PRECISION = 1000;//There are 2 copies of this
+  int MAX_EXP = 6, EXP_TABLE_PRECISION = 1000;
   int TABLE_SIZE = MAX_EXP * EXP_TABLE_PRECISION * 2;
   TFltV ExpTable(TABLE_SIZE);
   double Alpha, StartAlpha;
@@ -182,13 +228,16 @@ void LearnEmbeddings(TIntVV& WalksVV, int& Dimensions, int& WinSize,
     ExpTable[i] = TMath::Power(TMath::E, Value);
   }
   int WordCntAll = 0;
-  for (int i = 0; i < WalksVV.GetXDim(); i++) {
-    TIntV CurrV(WalksVV.GetYDim());
-    for (int j = 0; j < WalksVV.GetYDim(); j++) { CurrV[j] = WalksVV(i,j); }
-    TrainModel(CurrV, Vocab, Dimensions, WinSize, Iter, Rnd, KTable, UTable,
-     WordCntAll, ExpTable, Alpha, StartAlpha, WalksVV.GetXDim()*WalksVV.GetYDim(),
-     SynNeg, SynPos);
+  pthread_t* Threads =(pthread_t*) malloc(Workers*sizeof(pthread_t));//[Workers];
+  TThreadDat** ThreadDat =(TThreadDat**) malloc(Workers*sizeof(TThreadDat*));//[Workers];
+  for (int i = 0; i < Workers; i++) {
+    ThreadDat[i] = new TThreadDat(WalksVV, Dimensions, WinSize, Iter, Rnd, KTable, UTable,
+     WordCntAll, ExpTable, Alpha, StartAlpha, MAX_EXP, EXP_TABLE_PRECISION,
+     TABLE_SIZE, i*WalksVV.GetXDim()/Workers, (i+1)*WalksVV.GetXDim()/Workers - 1,
+     SynNeg, SynPos); 
+    pthread_create(&Threads[i], NULL, TrainModel, (void *)ThreadDat[i]);
   }
+  for (int i = 0; i < Workers; i++) { pthread_join(Threads[i], NULL); }
   for (int i = 0; i < SynPos.GetXDim(); i++) {
     TFltV CurrV(SynPos.GetYDim());
     for (int j = 0; j < SynPos.GetYDim(); j++) { CurrV[j] = SynPos(i, j); }
