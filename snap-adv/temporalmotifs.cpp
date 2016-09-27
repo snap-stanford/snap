35fc83fedcee6df9eebbafa27b5cfb4fa9023189
Austin Benson <arbenson@gmail.com>
2016-09-27 16:09:40 -0700

    Abstract away counting objects.

--- snap-adv/temporalmotifs.cpp
+++ snap-adv/temporalmotifs.cpp
@@ -1,13 +1,16 @@
 #include "Snap.h"
 #include "temporalmotifs.h"
 
+// TODO(arbenson): remove this cassert
+#include <cassert>
+
 const int kNumThreeEventEdgeMotifs = 4;
 
 void TemporalMotifCounter::LoadData(const TStr& filename) {
   // First load the static graph
   static_graph_ = TSnap::LoadEdgeList<PNGraph>(filename, 0, 1);
-  max_nodes_ = static_graph_->GetMxNId();
-  temporal_data_ = TVec< THash<TInt, TIntV> >(max_nodes_);
+  int max_nodes = static_graph_->GetMxNId();
+  temporal_data_ = TVec< THash<TInt, TIntV> >(max_nodes);
 
   // Formulate input File Format:
   //   source_node destination_node timestamp
@@ -32,7 +35,7 @@ void TemporalMotifCounter::LoadData(const TStr& filename) {
   }
 }
 
-void TemporalMotifCounter::ThreeEventEdgeMotifCounts(double delta, TIntV& counts) {
+void TemporalMotifCounter::ThreeEventEdgeCounts(double delta, TIntV& counts) {
   // Get a vector of undirected edges (so we can use openmp over it)
   TVec< TKeyDat<TInt, TInt> > undir_edges;
   for (TNGraph::TEdgeI it = static_graph_->BegEI();
@@ -53,7 +56,7 @@ void TemporalMotifCounter::ThreeEventEdgeMotifCounts(double delta, TIntV& counts
     TIntV local_counts;
     int src = edge.Key;
     int dst = edge.Dat;
-    ThreeEventEdgeMotifCounts(src, dst, delta, local_counts);
+    ThreeEventEdgeCounts(src, dst, delta, local_counts);
     if (local_counts.Len() != counts.Len()) {
       TExcept::Throw("Edge counts are of the incorrect size");
     }
@@ -66,8 +69,13 @@ void TemporalMotifCounter::ThreeEventEdgeMotifCounts(double delta, TIntV& counts
   }
 }
 
-void TemporalMotifCounter::ThreeEventEdgeMotifCounts(int u, int v, double delta,
-                                                     TIntV& counts) {
+void TemporalMotifCounter::ThreeEventStarCounts(double delta, TIntV& counts) {
+  int max_nodes = static_graph_->GetMxNId();  
+  ThreeEventStarMotifCounter tesmc(max_nodes);
+}
+
+void TemporalMotifCounter::ThreeEventEdgeCounts(int u, int v, double delta,
+						TIntV& counts) {
   // Get u --> v and v --> u
   TIntV& ts_uv = temporal_data_[u](v);
   TIntV& ts_vu = temporal_data_[v](u);
@@ -105,17 +113,8 @@ ThreeEventMotifCounter::ThreeEventMotifCounter(int size) {
   // Initialize everything to empty
   counts1_ = TIntV(size_);
   counts1_.PutAll(0);
-  counts2_ = TVec< TIntV >(size_);
-  counts3_ = TVec< TVec< TIntV > >(size_);
-  for (int i = 0; i < size_; i++) {
-    counts2_[i] = TIntV(size_);
-    counts2_[i].PutAll(0);
-    counts3_[i] = TVec< TIntV >(size_);
-    for (int j = 0; j < size_; j++) {
-      counts3_[i][j] = TIntV(size_);
-      counts3_[i][j].PutAll(0);
-    }
-  }
+  counts2_ = Counter2D(size_, size_);
+  counts3_ = Counter3D(size_, size_, size_);
 }
 
 void ThreeEventMotifCounter::Count(const TIntV& event_string,
@@ -138,12 +137,12 @@ void ThreeEventMotifCounter::IncrementCounts(int event) {
   // Update three-counts
   for (int i = 0; i < size_; i++) {
     for (int j = 0; j < size_; j++) {
-      counts3_[i][j][event] += counts2_[i][j];
+      counts3_(i, j, event) += counts2_(i, j);
     }
   }
   // Update two-counts
   for (int i = 0; i < size_; i++) {
-    counts2_[i][event] += counts1_[i];
+    counts2_(i, event) += counts1_[i];
   }
   // Update one-counts
   counts1_[event] += 1;
@@ -152,14 +151,137 @@ void ThreeEventMotifCounter::IncrementCounts(int event) {
 void ThreeEventMotifCounter::DecrementCounts(int event) {
   // Update one-counts
   counts1_[event]--;
-  if (!(counts1_[event] >= 0)) {
+  if (counts1_[event] < 0) {
     TExcept::Throw("Bad counts1 value in DecrementCounts()");
   }
   // Update two-counts
   for (int i = 0; i < size_; i++) {
-    counts2_[event][i] -= counts1_[i];
-    if (!(counts2_[event][i] >= 0)) {
+    counts2_(event, i) -= counts1_[i];
+    if (counts2_(event, i) < 0) {
       TExcept::Throw("Bad count in DecrementCounts()");
+    }
+  }
+}
+
+ThreeEventStarMotifCounter::ThreeEventStarMotifCounter(int num_nodes) {
+  // Initialize node counters
+  pre_nodes_ = TVec< TIntV >(2);
+  post_nodes_ = TVec< TIntV >(2);
+  for (int i = 0; i < 2; i++) {
+    pre_nodes_[i] = TIntV(num_nodes);
+    post_nodes_[i] = TIntV(num_nodes);
+    pre_nodes_[i].PutAll(0);
+    post_nodes_[i].PutAll(0);
+  }
+
+  // Initialize sum counters
+  pre_sum_ = TVec< TIntV >(2);
+  post_sum_ = TVec< TIntV >(2);
+  for (int i = 0; i < 2; i++) {
+    pre_sum_[i] = TIntV(2);
+    post_sum_[i] = TIntV(2);
+    pre_sum_[i].PutAll(0);
+    post_sum_[i].PutAll(0);
+  }
+
+  // Initialize final counters
+  pre_counts_ = TVec< TVec< TIntV > >(2);
+  post_counts_ = TVec< TVec< TIntV > >(2);
+  mid_counts_ = TVec< TVec< TIntV > >(2);  
+  for (int i = 0; i < 2; i++) {
+    pre_counts_[i] = TVec< TIntV >(2);
+    post_counts_[i] = TVec< TIntV >(2);
+    mid_counts_[i] = TVec< TIntV >(2);    
+    for (int j = 0; j < 2; j++) {
+      pre_counts_[i][j] = TIntV(2);
+      post_counts_[i][j] = TIntV(2);
+      mid_counts_[i][j] = TIntV(2);
+      pre_counts_[i][j].PutAll(0);
+      post_counts_[i][j].PutAll(0);
+      mid_counts_[i][j].PutAll(0);      
+    }
+  }
+}
+
+void ThreeEventStarMotifCounter::Count(const TIntV& nbr, const TIntV& dir,
+				       const TIntV& timestamps, double delta) {
+  if (dir.Len() != timestamps.Len() || dir.Len() != nbr.Len()) {
+    TExcept::Throw("nbr, dir, and timestamp vector must be the same size");
+  }
+  int start = 0;
+  int end = 0;
+  int L = timestamps.Len();
+  for (int j = 0; j < L; j++) {
+    double tj = double(timestamps[j]);
+    // Adjust counts in pre-window [tj - delta, tj)
+    while (start < L && double(timestamps[start]) + delta < tj) {
+      PopPre(nbr[start], dir[start]);
+      start++;
+    }
+    // Adjust counts in post-window (tj, tj + delta]
+    PopPost(nbr[j], dir[j]);
+    DecMiddleSum(nbr[j], dir[j]);
+    while (end < L && double(timestamps[end]) < tj + delta) {
+      PushPost(nbr[end], dir[end]);
+      end++;
+    }
+    // Handle the current event
+    ProcessCurrent(nbr[j], dir[j]);
+    // Put event in pre-window
+    PushPre(nbr[j], dir[j]);
+    IncMiddleSum(nbr[j], dir[j]);
+  }
+}
+
+void ThreeEventStarMotifCounter::PopPre(int nbr, int dir) {
+  pre_nodes_[dir][nbr] -= 1;
+  assert(pre_nodes_[dir][nbr] >= 0);
+  for (int i = 0; i < 2; i++) {
+    pre_sum_[i][dir] -= pre_nodes_[dir][nbr];
+  }
+}
+
+void ThreeEventStarMotifCounter::PopPost(int nbr, int dir) {
+  post_nodes_[dir][nbr] -= 1;
+  assert(post_nodes_[dir][nbr] >= 0);  
+  for (int i = 0; i < 2; i++) {
+    post_sum_[i][dir] -= post_nodes_[dir][nbr];
+  }
+}
+
+void ThreeEventStarMotifCounter::PushPre(int nbr, int dir) {
+  for (int i = 0; i < 2; i++) {
+    pre_sum_[i][dir] += pre_nodes_[dir][nbr];
+  }
+  pre_nodes_[dir][nbr] += 1;
+}
+
+void ThreeEventStarMotifCounter::PushPost(int nbr, int dir) {
+  for (int i = 0; i < 2; i++) {
+    post_sum_[i][dir] += post_nodes_[dir][nbr];
+  }
+  post_nodes_[dir][nbr] += 1;
+}
+
+void ThreeEventStarMotifCounter::DecMiddleSum(int nbr, int dir) {
+  for (int i = 0; i < 2; i++) {
+    mid_sum_[i][dir] -= pre_nodes_[dir][nbr];
+    assert(mid_sum_[i][dir] >= 0);
+  }
+}
+
+void ThreeEventStarMotifCounter::IncMiddleSum(int nbr, int dir) {
+  for (int i = 0; i < 2; i++) {
+    mid_sum_[i][dir] += post_nodes_[dir][nbr];
+  }
+}
+
+void ThreeEventStarMotifCounter::ProcessCurrent(int nbr, int dir) {
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      pre_counts_[i][j][dir] += pre_sum_[i][j];
+      post_counts_[dir][i][j] += post_sum_[i][j];
+      mid_counts_[i][dir][j] += mid_sum_[i][j];      
     }
   }
 }
--- snap-adv/temporalmotifs.h
+++ snap-adv/temporalmotifs.h
@@ -1,41 +1,161 @@
 #ifndef snap_temporalmotifs_h
 #define snap_temporalmotifs_h
+
 #include "Snap.h"
 #include "motifcluster.h"
+
+class Counter2D {
+ public:
+  // Base constructor
+  Counter2D(int m, int n) : m_(m), n_(n) {
+    if (m * n > 0) {
+      TIntV data_ = TIntV(m * n);
+      data_.PutAll(0);
+    }
+  }
+  Counter2D() : Counter2D(0, 0) {}
+  // Copy assignment
+  Counter2D& operator=(const Counter2D& that) {
+    if (this != &that) {
+      m_ = that.m();
+      n_ = that.n();
+      if (m_ * n_ > 0) {
+	data_ = TIntV(m_ * n_);
+	for (int j = 0; j < n_; ++j) {
+	  for (int i = 0; i < m_; ++i) {
+	    data_[i + j * m_] = that(i, j);
+	  }
+	}
+      } else {
+	data_ = TIntV();
+      }
+    }
+    return *this;
+  }
+
+  int m() const { return m_; }
+  int n() const { return n_; }
+  
+  const TInt& operator()(int i, int j) const { return data_[i + j * m_]; }
+  TInt& operator()(int i, int j) { return data_[i + j * m_]; }
+
+ private:
+  int m_;
+  int n_;
+  TIntV data_;
+};
+
+class Counter3D {
+ public:
+  // Base constructor
+  Counter3D(int m, int n, int p) : m_(m), n_(n), p_(p) {
+    if (m * n * p > 0) {
+      TIntV data_ = TIntV(m * n * p);
+      data_.PutAll(0);
+    }
+  }
+  Counter3D() : Counter3D(0, 0, 0) {}
+  // Copy assignment
+  Counter3D& operator=(const Counter3D& that) {
+    if (this != &that) {
+      m_ = that.m();
+      n_ = that.n();
+      p_ = that.p();
+      if (m_ * n_ * p_ > 0) {
+	data_ = TIntV(m_ * n_ * p_);
+	for (int k = 0; k < p_; ++k) {      
+	  for (int j = 0; j < n_; ++j) {
+	    for (int i = 0; i < m_; ++i) {
+	      data_[i + j * m_ + k * m_ * n_] = that(i, j, k);
+	    }
+	  }
+	}
+      } else {
+	data_ = TIntV();
+      }
+    }
+    return *this;
+  }
+  
+  int m() const { return m_; }
+  int n() const { return n_; }
+  int p() const { return p_; }
+  TIntV& data() { return data_; }
+  
+  const TInt& operator()(int i, int j, int k) const {
+    return data_[i + j * m_ + k * m_ * n_];
+  }
+  TInt& operator()(int i, int j, int k) {
+    return data_[i + j * m_ + k * m_ * n_];
+  }
+
+ private:
+  int m_;
+  int n_;
+  int p_;
+  TIntV data_;
+};
 
 class TemporalMotifCounter {
  public:
   TemporalMotifCounter() {}
   void LoadData(const TStr& filename);
 
-  void ThreeEventEdgeMotifCounts(double delta, TIntV& counts);
-  void ThreeEventEdgeMotifCounts(int u, int v, double delta,
-                                 TIntV& counts);
+  void ThreeEventEdgeCounts(double delta, TIntV& counts);
+  void ThreeEventEdgeCounts(int u, int v, double delta,
+			    TIntV& counts);
+
+  void ThreeEventStarCounts(double delta, TIntV& counts);
 
  private:
   PNGraph static_graph_;
-  int max_nodes_ = -1;
-  TIntV timestamps_;
   // temporal_data_[i][j] --> list of timestamps on (i, j)
   // This data structure double stores to access (i, j) or (j, i)
   TVec< THash<TInt, TIntV> > temporal_data_;
 };
 
 class ThreeEventMotifCounter {
-public:
+ public:
   ThreeEventMotifCounter(int size);
   void Count(const TIntV& event_string, const TIntV& timestamps,
              double delta);
-  int FinalCount(int i, int j, int k) { return counts3_[i][j][k]; }  
+  int FinalCount(int i, int j, int k) { return counts3_(i, j, k); }  
 
-private:
+ private:
   void IncrementCounts(int event);
   void DecrementCounts(int event);
   TIntV counts1_;
-  TVec< TIntV > counts2_;
-  TVec< TVec< TIntV > > counts3_;
+  Counter2D counts2_;
+  Counter3D counts3_;
   int size_;  // alphabet size
 };
 
+class ThreeEventStarMotifCounter {
+ public:
+  ThreeEventStarMotifCounter(int num_nodes);
+
+  void Count(const TIntV& node, const TIntV& dir,
+	     const TIntV& timestamps, double delta);
+ private:
+  void PopPre(int nbr, int dir);
+  void PopPost(int nbr, int dir);
+  void PushPre(int nbr, int dir);
+  void PushPost(int nbr, int dir);
+  void DecMiddleSum(int nbr, int dir);
+  void IncMiddleSum(int nbr, int dir);
+  void ProcessCurrent(int nbr, int dir);  
+
+
+  TVec< TIntV > pre_sum_;
+  TVec< TIntV > post_sum_;  
+  TVec< TIntV > mid_sum_;
+  
+  TVec< TVec< TIntV > > pre_counts_;
+  TVec< TVec< TIntV > > post_counts_;
+  TVec< TVec< TIntV > > mid_counts_;
+
+  TVec< TIntV > pre_nodes_;
+  TVec< TIntV > post_nodes_;  
+};
 
 #endif  // snap_temporalmotifs_h
