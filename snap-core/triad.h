0276385263c701366e59c8c48ce3ca5ba5c5b4cf
Rok Sosic <rok@cs.stanford.edu>
2018-07-24 18:01:07 -0700

    - fixed a compilation error on MacOS

--- snap-core/triad.h
+++ snap-core/triad.h
@@ -22,7 +22,7 @@ template <class PGraph> void GetNodeClustCf(const PGraph& Graph, TIntFltH& NIdCC
 /// Returns the number of triangles in a graph. ##TSnap::GetTriads
 template <class PGraph> int64 GetTriads(const PGraph& Graph, int SampleNodes=-1);
 /// Computes the number of Closed and Open triads. ##TSnap::GetTriads1
-template <class PGraph> int64 GetTriads(const PGraph& Graph, int64& ClosedTriadsX, int64& OpenTriadsX, int SampleNodes=-1);
+template <class PGraph> int64 GetTriads(const PGraph& Graph, int64& ClosedTriadsX, int64& OpenTriadsX, int SampleNodes);
 /// Computes the number of Closed and Open triads. ##TSnap::GetTriadsAll
 template <class PGraph> int64 GetTriadsAll(const PGraph& Graph, int64& ClosedTriadsX, int64& OpenTriadsX, int SampleNodes=-1);
 /// Computes the number of open and close triads for every node of the network. ##TSnap::GetTriads2
