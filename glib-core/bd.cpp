ac89abd709cec9205cc24edbc9cc9dda789102c2
valentin Bisson <val.bisson@gmail.com>
2018-10-19 19:53:21 -0400

    build snapcore and testSnap

--- glib-core/bd.cpp
+++ glib-core/bd.cpp
@@ -1,15 +1,23 @@
 #if SW_TRACE
 #include <execinfo.h>
 #endif
-
+#include <math.h>
 /////////////////////////////////////////////////
 // Mathmatical-Errors
+
 #if defined(__BCPLUSPLUS__) && (__BCPLUSPLUS__==0x0530)
 int std::_matherr(struct math_exception* e){
   e->retval=0;
   return 1;
 }
 #elif defined(GLib_GLIBC) || defined(GLib_BSD)
+struct __exception {
+    int    type;      /* Exception type */
+    char*  name;      /* Name of function causing exception */
+    double arg1;      /* 1st argument to function */
+    double arg2;      /* 2nd argument to function */
+    double retval;    /* Function return value */
+};
 int _matherr(struct __exception* e){
   e->retval=0;
   return 1;
