36dce386f1fd07349e77efb306648e9c622292a0
valentin Bisson <val.bisson@gmail.com>
2018-10-21 15:27:58 -0400

    #minor cleanup

--- cmake/windows_compat.cpp
+++ cmake/windows_compat.cpp
@@ -3,7 +3,7 @@
 // only defined in stdlib.h
 double drand48()
 {
-	unsigned int randomValue;
-	rand_s(&randomValue);
-	return ((double)randomValue) / UINT_MAX;
+    unsigned int randomValue;
+    rand_s(&randomValue);
+    return ((double)randomValue) / UINT_MAX;
 }
